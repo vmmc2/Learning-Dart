904f711d0a99a347891f79b7740c5d71d6b0df37
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-12-23 19:26:31 -0300

    Create final_and_const.dart

---
+++ final_and_const.dart
@@ -0,0 +1,9 @@
+void main(){
+    final String nome = "Victor";
+    nome = "Dale"; //Nao pode mudar uma variavel do tipo final
+    const String dad = "Andros";
+    dad = "Misera"; //Mesma coisa vale para uma variavel do tipo const. Nao pode mudar...
+    print(nome);
+    print(dad);
+    
+}
