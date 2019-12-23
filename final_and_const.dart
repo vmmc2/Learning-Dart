17cd70965a18ef17aea2cb662df9a64177d4d846
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-12-23 19:48:50 -0300

    Update final_and_const.dart

--- final_and_const.dart
+++ final_and_const.dart
@@ -1,9 +1,16 @@
 void main(){
     final String nome = "Victor";
-    nome = "Dale"; //Nao pode mudar uma variavel do tipo final
+    //nome = "Dale"; //Nao pode mudar uma variavel do tipo final
     const String dad = "Andros";
-    dad = "Misera"; //Mesma coisa vale para uma variavel do tipo const. Nao pode mudar...
+    //dad = "Misera"; //Mesma coisa vale para uma variavel do tipo const. Nao pode mudar...
     print(nome);
     print(dad);
-    
+    //Se for usar uma variavel const dentro de uma classe, use static const..
+    const bar = 100000;
+    const double atm = 1.01325 * bar;
+    //Testando const e lists..
+    final lista = [1,2,3];
+    print(lista);
+    lista.add(6);
+    print(lista);
 }
