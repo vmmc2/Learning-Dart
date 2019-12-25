46d5b4c54617aa467e618f636d196056ee96e7f2
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-12-25 18:35:15 -0300

    Create Arithmetic_Operators.dart

---
+++ Arithmetic_Operators.dart
@@ -0,0 +1,15 @@
+//Arithmetic Operators in Dart
+
+//-expr ->	Unary minus, also known as negation (reverse the sign of the expression)
+// *  -> Multiply
+// /  -> Divide
+// ~/ -> Divide, returning an integer result
+// %  -> Get the remainder of an integer division (modulo)
+
+void main(){
+  int a = 7;
+  int b = 4;
+  int answer = (a)~/(b); //fica melhor pra legibilidade... (na minha opiniao)
+  print(answer);
+  return;
+}
