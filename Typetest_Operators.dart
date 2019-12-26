6581e2c4112ad78ed6c6f280c4aa2dccd69757ac
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-12-26 16:31:15 -0300

    Update Typetest_Operators.dart

--- Typetest_Operators.dart
+++ Typetest_Operators.dart
@@ -9,3 +9,12 @@ The result of: obj is T is true if obj implements the interface specified by T
 For example: obj is Object is always true.
 
 As we said above... we can use as to make a typecast to a specific type...
+  
+//Example os using is and then making an assignment...
+if (emp is Person) {
+  // Type check
+  emp.firstName = 'Bob';
+}
+
+//Example of direclty using the "as" typetest operator...
+(emp as Person).firstName = 'Bob';
