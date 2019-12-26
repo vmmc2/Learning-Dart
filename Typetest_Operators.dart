179ab22535c6686b80414fe5ae18d4673bb219ec
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2019-12-26 16:27:52 -0300

    Create Typetest_Operators.dart

---
+++ Typetest_Operators.dart
@@ -0,0 +1,11 @@
+//Type test operators
+//We have these type test operators: "as", "is" and "is!"...
+
+as  -> typecast (also used to specify library prefixes)
+is  -> true if the object has the specified type 
+is! -> false if the object has the specified type
+
+The result of: obj is T is true if obj implements the interface specified by T 
+For example: obj is Object is always true.
+
+As we said above... we can use as to make a typecast to a specific type...
